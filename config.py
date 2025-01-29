1d7bf420a5f98d0c60769ef418d247492d498194
krishan041 <krishandara04@gmail.com>
2025-01-29 17:46:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", '7929130193:AAFJswIMtSV_CPazyAPKW4BQ58ijov3t-5I')
-    API_ID = int(os.environ.get("API_ID", '28094744'))
-    API_HASH = os.environ.get("API_HASH", 'a75af4285edc7747c57bb19147ca0b9b')
-    AUTH_USER = os.environ.get('AUTH_USERS','5680454765').split(',')
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", '')
+    API_ID = int(os.environ.get("API_ID", '23442913'))
+    API_HASH = os.environ.get("API_HASH", '864a97e16b4ff7dc65ff5e2d1549b4a2')
+    AUTH_USER = os.environ.get('AUTH_USERS','7841326954').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     WEBHOOK = True  # Don't change this
     HOST = "https://drm-api-six.vercel.app"
