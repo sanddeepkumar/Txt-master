579fac49b7d16b00e1130a856199ce3bc4f8a2be
manishadara04 <manishadara04@gmail.com>
2025-02-01 16:59:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", '864a97e16b4ff7dc65ff5e2d1549b4a2')
     AUTH_USER = os.environ.get('AUTH_USERS','7841326954').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    WEBHOOK = false  # Don't change this
     CREDIT = "🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌"#Here You Can Change with Your Name  or any custom name or title you prefer
     port = int(os.environ.get('PORT', 8080))  # Default to 8000 for local testing
 
